5a58e9780897172614234264f77608d4649dd288
unknown <mackovckin.sergei@yandex.ru>
2020-10-10 23:04:28 +0300

    heroku push

--- projectbase/settings.py
+++ projectbase/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 #For heroku configyration
-#import django_heroku
+import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,10 +106,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
+
 #For heroku configuration
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
